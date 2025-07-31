6058cb87a239a8d6cb366f506e3de1240079a016
Felipe Rivetti <fporto@sarrat.com.br>
2025-07-30 21:31:08 -0300

    Refactor app layout

--- src/app.py
+++ src/app.py
@@ -3,10 +3,14 @@ import components, queries
 
 
 def main():
-    # Configuração da página
-    st.set_page_config(layout="centered", page_title="Dashboard de Vendas", page_icon=":bar_chart:")
+    # --- Configuração da Página ---
+    st.set_page_config(
+        layout="centered",
+        page_title="Dashboard de Vendas",
+        page_icon="📈"
+    )
 
-    st.title("Dashboard de Análise de Vendas")
+    st.title("📈 Dashboard - Análise de Vendas")
 
     # --- Exibe os KPIs principais ---
     components.card_kpi(
@@ -17,19 +21,55 @@ def main():
         tempo_medio_entrega=queries.get_average_delivery_time(),
         total_clientes=queries.get_total_customers(),
     )
-    st.write("---")
 
-    # --- Análise de Vendas por Cidade, Estado ou Setor ---
-    components.display_sales_by_category_pie_chart()
-    st.write("---")
+    # --- Criação das Abas ---
+    # Vamos criar duas abas, uma para cada contexto de análise
+    tab1, tab2 = st.tabs(["📊 Análise de Vendas", "🚚 Análise de Entregas"])
 
-    # --- Análise de Vendas por Período ---
-    components.display_orders_analysis()
-    st.write("---")
+    # --- Conteúdo da Aba 1: Análise de Vendas ---
+    with tab1:
+        st.header("Visão Geral das Vendas")
 
-    # --- Análise de Tempo de Entrega ---
-    components.display_delivery_time_histogram()
+        components.display_orders_analysis()
+
+        components.display_sales_by_category_pie_chart()
+        
+
+    # --- Conteúdo da Aba 2: Análise de Entregas ---
+    with tab2:
+        st.header("Performance da Logística e Satisfação do Cliente")
+
+        # Análise de Tempo de Entrega (Histograma)
+        components.display_delivery_time_histogram()
+
+        # Análise de Correlação: Tempo de Entrega vs. Avaliação
+        components.display_correlation_boxplot()
+    
+    
+    # # --- Conteúdo da Aba 4: Previsão ---
+    # with tab3:
+    #     # Componente de previsão de faturamento
+    #     components.display_revenue_forecast()
+
+    # --- Conteúdo da Aba 3: Análise Geográfica ---
+    # with tab4:
+    #     # Mapa interativo que criamos
+    #     components.display_customer_map_by_city()
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
+# # --- Exibe os KPIs principais ---
+#     components.card_kpi(
+#         faturamento_total=queries.get_faturamento_total(),
+#         total_pedidos=queries.get_total_orders(), 
+#         ticket_medio=queries.get_ticket_medio(),
+#         avaliacao_media=queries.get_average_rating(),
+#         tempo_medio_entrega=queries.get_average_delivery_time(),
+#         total_clientes=queries.get_total_customers(),
+#     )
+#     st.write("---")
